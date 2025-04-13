345992a52b7162a1b28a2fdbc1774fc01fd5f29d
kotilja <komandanto228@gmail.com>
2025-04-14 01:21:07 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
-import time
+from datetime import time as dt_time
 
 TOKEN = os.getenv("TOKEN")
 YOUR_USER_ID = int(os.getenv("YOUR_USER_ID"))
 CHANNEL_ID = os.getenv("CHANNEL_ID")
+
 POST_INTERVAL = int(os.getenv("POST_INTERVAL", 3600))
-HASHTAG = '#ea7randompic'
-# Временной интервал публикаций
-START_TIME = dt_time(int(os.getenv("START_TIME")), 0)  
-END_TIME = dt_time(int(os.getenv("END_TIME")), 0)    
+HASHTAG = os.getenv("HASHTAG", '#ea7randompic')
+
+START_TIME = dt_time(int(os.getenv("START_TIME", 10)), 0)  # По умолчанию: 10:00
+END_TIME = dt_time(int(os.getenv("END_TIME", 23)), 0)      # По умолчанию: 23:00
