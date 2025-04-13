52fa45062190b107420eb8d0d2e13025d23ec33e
kotilja <komandanto228@gmail.com>
2025-04-14 01:05:54 +0400

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 TOKEN = os.getenv("TOKEN")
 YOUR_USER_ID = int(os.getenv("YOUR_USER_ID"))
 CHANNEL_ID = os.getenv("CHANNEL_ID")
-POST_INTERVAL = int(os.getenv("POST_INTERVAL", 60))
+POST_INTERVAL = int(os.getenv("POST_INTERVAL", 3600))
 HASHTAG = '#ea7randompic'
