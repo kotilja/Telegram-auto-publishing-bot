eb5572a1c33cbc7b83c5d015dadb2fb464b8ee4a
kotilja <komandanto228@gmail.com>
2025-04-14 02:33:26 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ from datetime import time as dt_time
 TOKEN = os.getenv("TOKEN")
 YOUR_USER_ID = int(os.getenv("YOUR_USER_ID"))
 CHANNEL_ID = os.getenv("CHANNEL_ID")
-
 POST_INTERVAL = int(os.getenv("POST_INTERVAL", 3600))
 HASHTAG = os.getenv("HASHTAG", '#ea7randompic')
 
