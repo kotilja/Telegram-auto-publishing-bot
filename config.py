e55039fe4018c40271ccb076b78134068c60a455
kotilja <komandanto228@gmail.com>
2025-04-14 01:15:21 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+import time
 
 TOKEN = os.getenv("TOKEN")
 YOUR_USER_ID = int(os.getenv("YOUR_USER_ID"))
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 POST_INTERVAL = int(os.getenv("POST_INTERVAL", 3600))
 HASHTAG = '#ea7randompic'
+# Временной интервал публикаций
+START_TIME = dt_time(int(os.getenv("START_TIME")), 0)  
+END_TIME = dt_time(int(os.getenv("END_TIME")), 0)    
