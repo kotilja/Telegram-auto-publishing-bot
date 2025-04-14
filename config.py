070324e85ad3462f6decf70f4d258cc27e964e91
kotilja <komandanto228@gmail.com>
2025-04-14 20:47:52 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ YOUR_USER_ID = int(os.getenv("YOUR_USER_ID"))
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 POST_INTERVAL = int(os.getenv("POST_INTERVAL", 3600))
 HASHTAG = os.getenv("HASHTAG", '#ea7randompic')
+REDIS_BROKER = os.getenv("REDIS_BROKER")
 
-START_TIME = dt_time(int(os.getenv("START_TIME", 10)), 0)  # По умолчанию: 10:00
-END_TIME = dt_time(int(os.getenv("END_TIME", 23)), 0)      # По умолчанию: 23:00
+START_TIME = dt_time(int(os.getenv("START_TIME", 8)), 0)
+END_TIME = dt_time(int(os.getenv("END_TIME", 21)), 0)
